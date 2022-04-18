19ce2c479150da489b25f288cb0f8a6c287b0901
Gabrielhxg <52026781+Gabrielhxg@users.noreply.github.com>
2022-04-17 21:11:43 -0300

    last one

--- my_apps/support.py
+++ my_apps/support.py
@@ -7,7 +7,7 @@ from google.oauth2 import service_account
 from google.cloud import bigquery
 
 ################## Create API client ##################
-credentials = service_account.Credentials.from_service_account_info(st.mySecrets['gcp_service_account'])
+credentials = service_account.Credentials.from_service_account_info(st.secrets['gcp_service_account'])
 client = bigquery.Client(credentials=credentials)
 
 ################# Define function to hide stuff #######
